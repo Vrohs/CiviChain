d313b1118c52007e7d657ab76e6800d8e91bb4b1
Vrohs <info.rohsgaming536@gmail.com>
2024-09-02 00:35:55 +0530

    refactor: Update UserVotes contract to use SPDX-License-Identifier and pragma solidity version 0.8.0

--- contracts/UserVotes.sol
+++ contracts/UserVotes.sol
@@ -1,34 +1,39 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.24;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-//  Uncomment this line to use console.log
-// import "hardhat/console.sol";
-
-contract Lock {
-    uint public unlockTime;
-    address payable public owner;
-
-    event Withdrawal(uint amount, uint when);
-
-    constructor(uint _unlockTime) payable {
-        require(
-            block.timestamp < _unlockTime,
-            "Unlock time should be in the future"
-        );
-
-        unlockTime = _unlockTime;
-        owner = payable(msg.sender);
+contract Voting {
+    struct Candidate {
+        string name;
+        uint256 voteCount;
     }
 
-    function withdraw() public {
-        // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
-        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
+    mapping(address => bool) public voters;
+    Candidate[] public candidates;
 
-        require(block.timestamp >= unlockTime, "You can't withdraw yet");
-        require(msg.sender == owner, "You aren't the owner");
+    constructor(string[] memory _candidateNames) {
+        for (uint256 i = 0; i < _candidateNames.length; i++) {
+            candidates.push(Candidate({
+                name: _candidateNames[i],
+                voteCount: 0
+            }));
+        }
+    }
 
-        emit Withdrawal(address(this).balance, block.timestamp);
+    function vote(uint256 _candidateIndex) public {
+        require(!voters[msg.sender], "You have already voted.");
+        require(_candidateIndex < candidates.length, "Invalid candidate index.");
 
-        owner.transfer(address(this).balance);
+        voters[msg.sender] = true;
+        candidates[_candidateIndex].voteCount++;
+    }
+
+    function getWinner() public view returns (string memory winnerName) {
+        uint256 winningVoteCount = 0;
+        for (uint256 i = 0; i < candidates.length; i++) {
+            if (candidates[i].voteCount > winningVoteCount) {
+                winningVoteCount = candidates[i].voteCount;
+                winnerName = candidates[i].name;
+            }
+        }
     }
 }
