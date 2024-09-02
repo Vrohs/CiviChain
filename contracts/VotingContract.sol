af142332abd734f4b394fa955c72925d3df3fbdf
Vrohs <info.rohsgaming536@gmail.com>
2024-09-02 14:10:33 +0530

    refactor: Add owner and votingEnded variables to VotingContract

--- contracts/VotingContract.sol
+++ contracts/VotingContract.sol
@@ -2,44 +2,58 @@
 pragma solidity ^0.8.0;
 
 contract VotingContract {
+
     struct Candidate {
+
         string name;
         uint256 voteCount;    // unsigned c-like 256 bit integer var 
     }
 
-    mapping(address => bool) public voters;
+// required variables 
+    mapping(address => bool) public voters;       // key value relation for determining if a user has voted 
     mapping(address => bool) public hasVoted;
-    Candidate[] public candidates;
-    bool public votingEnded;
+    Candidate[] public candidates;                // ar of candidates
+    bool public votingEnded;         
     address public owner;
 
+
     constructor(string[] memory _candidateNames) {
+
         for (uint256 i = 0; i < _candidateNames.length; i++) {
             candidates.push(Candidate({
                 name: _candidateNames[i],
                 voteCount: 0
             }));
         }
+
         owner = msg.sender;
         votingEnded = false;
     }
 
+
     modifier onlyOwner() {
+
         require(msg.sender == owner, "Only the contract owner can call this function");
         _;
     }
 
+
     modifier votingOngoing() {
+    
         require(!votingEnded, "Voting has ended");
         _;
     }
 
+
     function registerVoter() public votingOngoing {
+
         require(!voters[msg.sender], "You have already registered.");
         voters[msg.sender] = true;
     }
 
+
     function castVote(uint256 _candidateIndex) public votingOngoing {
+
         require(voters[msg.sender], "You must be registered to vote");
         require(!hasVoted[msg.sender], "You have already voted");
         require(_candidateIndex < candidates.length, "Invalid candidate index");
@@ -48,18 +62,25 @@ contract VotingContract {
         hasVoted[msg.sender] = true;
     }
 
+
     function getVoteCount(uint256 _candidateIndex) public view returns (uint256) {
+
         require(_candidateIndex < candidates.length, "Invalid candidate index");
         return candidates[_candidateIndex].voteCount;
     }
 
+
     function endVoting() public onlyOwner {
+
         votingEnded = true;
     }
 
+
     function getWinner() public view returns (string memory winnerName) {
+
         require(votingEnded, "Voting has not ended yet");
         uint256 winningVoteCount = 0;
+
         for (uint256 i = 0; i < candidates.length; i++) {
             if (candidates[i].voteCount > winningVoteCount) {
                 winningVoteCount = candidates[i].voteCount;
